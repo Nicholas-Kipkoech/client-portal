e1585e7cb847d8b933216b41c62984e35cb8b8f6
Nicholas Kipkoech <nickey968@gmail.com>
2024-04-17 16:13:33 +0300

    fix home responsive for mobile

--- app/components/Home.tsx
+++ app/components/Home.tsx
@@ -11,24 +11,24 @@ const HomePage = () => {
     setModalOpen(true);
   };
   return (
-    <div className="flex gap-10 justify-between items-center">
-      <div className="flex flex-col justify-start gap-10">
-        <p className="text-[2rem] font-[700] 2xl:text-[4rem] font-sans">
+    <div className="flex gap-10 justify-between sm:flex-col sm:gap-4 items-center">
+      <div className="flex flex-col justify-start gap-10 sm:gap-4">
+        <p className=" sm:text-[1rem] text-[2rem] font-[700] 2xl:text-[4rem] font-sans">
           We keep you covered, wherever you’re planted.
         </p>
 
-        <div className="border rounded-[30px] h-[60px] bg-white  outline-[#cb7529] flex justify-between px-5 items-center">
+        <div className="md:border rounded-[30px] h-[60px] sm:h-[12px] bg-white  outline-[#cb7529] flex justify-between px-5 items-center">
           <input
             type="text"
             placeholder="Enter your addresss"
-            className="h-[50px] w-full p-[5px]  outline-none "
+            className="h-[50px] w-full p-[5px] sm:hidden outline-none "
           />
 
           <CustomButton
             onClick={handleOpenQuote}
             name={"Get a Quote"}
             className={
-              "h-[3rem] bg-[#cb7529] rounded-[30px] w-[10rem] 2xl:w-[15rem] text-white"
+              "sm:h-[2rem] sm:rounded-[10px] sm:text-[13px] h-[3rem] bg-[#cb7529] rounded-[30px] w-[10rem] 2xl:w-[15rem] text-white"
             }
           />
         </div>
@@ -37,7 +37,7 @@ const HomePage = () => {
       <Image
         src={house}
         alt="house"
-        className="rounded-[20px] h-[20rem] 2xl:h-[25rem] w-[40rem]  2xl:w-[80rem]"
+        className="rounded-[20px] sm:h-[10rem] sm:w-[20rem] sm:rounded-sm h-[20rem] 2xl:h-[25rem] w-[40rem]  2xl:w-[80rem]"
       />
 
       <QuoteModal open={modalOpen} handleClose={() => setModalOpen(false)} />
