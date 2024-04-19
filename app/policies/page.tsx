32688a8fcf63897fa3ec3ce52dc07e2963a25047
Nicholas Kipkoech <nickey968@gmail.com>
2024-04-19 14:33:09 +0300

    add policies table

--- app/policies/page.tsx
+++ app/policies/page.tsx
@@ -1,4 +1,6 @@
 "use client";
+
+import { Table } from "antd";
 import { useRouter } from "next/navigation";
 import React from "react";
 import { GrPrevious } from "react-icons/gr";
@@ -11,32 +13,53 @@ const Policies = () => {
     router.push(`/policies/${name.replace(" ", "")}`);
   };
 
+  const columns = [
+    {
+      title: "Quote Number",
+      dataIndex: "invoiceNumber",
+      render: (_: any, item: string) => <p>Q/02/03258/04/2024</p>,
+    },
+    {
+      title: "End No",
+      dataIndex: "invoiceNumber",
+      render: (_: any, item: string) => <p>Q/02/03258/04/2024</p>,
+    },
+    {
+      title: "Product",
+      dataIndex: "invoiceNumber",
+      render: (_: any, item: string) => <p>Q/02/03258/04/2024</p>,
+    },
+    {
+      title: "Period",
+      dataIndex: "invoiceNumber",
+      render: (_: any, item: string) => <p>Q/02/03258/04/2024</p>,
+    },
+    {
+      title: "Entities",
+      dataIndex: "invoiceNumber",
+      render: (_: any, item: string) => <p>Q/02/03258/04/2024</p>,
+    },
+    {
+      title: "Status",
+      dataIndex: "invoiceNumber",
+      render: (_: any, item: string) => <p>Q/02/03258/04/2024</p>,
+    },
+  ];
+
   return (
     <div className="flex flex-col justify-center my-4 md:mx-8 sm:mx-4">
-      <div
-        className="flex my-4  items-center justify-between cursor-pointer "
-        onClick={() => router.back()}
-      >
-        <div className="flex gap-0 items-center">
+      <div className="flex justify-between">
+        <div
+          className="cursor-pointer flex items-center"
+          onClick={() => router.back()}
+        >
           <GrPrevious size={15} />
           <p>Back</p>
         </div>
-        <p className="md:text-[1.8rem] sm:text-[1.3rem] font-semibold">
-          Policies
-        </p>
+        <p>Policies</p>
         <p></p>
       </div>
-      <div className="flex justify-center ">
-        <div className="border h-[8rem] w-[100%] gap-2 rounded-md shadow-xl md:py-5 px-5 sm:py-3 sm:px-2 flex justify-center items-center flex-col">
-          <p>Sample Reciept </p>
-          <div
-            className="mx-2 md:h-[4rem] sm:h-[2.5rem] border md:w-[10rem] flex items-center cursor-pointer justify-center rounded-md bg-[#cb7529] text-white"
-            onClick={() => handleViewReceipt("Sample Receipt")}
-          >
-            View Reciept
-          </div>
-        </div>
-      </div>
+      <Table columns={columns} dataSource={[]} />
     </div>
   );
 };
