45732807dc8a90a90963b1c3b471a9cdcbeebeb4
Nicholas Kipkoech <nickey968@gmail.com>
2024-04-26 12:38:27 +0300

    design user div

--- app/components/HomeNavbar.tsx
+++ app/components/HomeNavbar.tsx
@@ -1,13 +1,25 @@
 "use client";
 
 import React from "react";
+import CustomButton from "../utils/CustomButtom";
+import { useRouter } from "next/navigation";
 
 const HomeNavbar = () => {
+  const router = useRouter();
+
+  const handleLogin = () => {
+    router.push("/auth/login");
+  };
   return (
     <div className="sticky top-0 bg-[#F7F5FD] flex justify-between z-10 md:p-[10px] sm:p-[1px] h-[5rem] ">
       <span className="flex md:text-[18px] sm:text-[14px] font-bold cursor-pointer">
         CLIENT PORTAL
       </span>
+      <CustomButton
+        name="Login"
+        onClick={handleLogin}
+        className="h-[2rem] w-[8rem]  bg-[#cb7529] rounded-md text-white"
+      />
     </div>
   );
 };
--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,88 +1,72 @@
 "use client";
 
-import React, { useEffect, useState } from "react";
-import Login from "./Login";
-import { GoArrowRight } from "react-icons/go";
-import QuoteModal from "./QuoteModal";
+import React, { useState } from "react";
 import { useContextApi } from "../context/context";
-import { usePathname, useRouter } from "next/navigation";
-import {
-  Menu,
-  MenuButton,
-  MenuList,
-  MenuItem,
-  IconButton,
-} from "@chakra-ui/react";
+import { useRouter } from "next/navigation";
 
-import { FaChevronDown } from "react-icons/fa";
-import { IoMenu } from "react-icons/io5";
-import { MdMenu } from "react-icons/md";
 import CustomButton from "../utils/CustomButtom";
+import { Button, Menu, MenuButton, MenuItem, MenuList } from "@chakra-ui/react";
+import { IoChevronDownCircleOutline } from "react-icons/io5";
 
 const Navbar = () => {
-  const [openLogin, setOpenLogin] = useState(false);
-  const [openModal, setOpenModal] = useState(false);
-  const { isUserAuthenticated, isMobile, user }: any = useContextApi();
-  const [openDrawer, setOpenDrawer] = useState(false);
+  const { user }: any = useContextApi();
 
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
   const router = useRouter();
-
-  const pathname = usePathname().replace("/", "");
-
-  const handleOpenModal = () => {
-    if (isMobile) {
-      router.push("/mobile/quote");
-    } else {
-      setOpenModal(true);
-    }
-  };
-  const handleLogin = () => {
-    if (isMobile) {
-      router.push("/mobile/auth/login");
-    } else {
-      setOpenLogin(true);
-    }
-  };
 
   const handleLogout = () => {
     localStorage.removeItem("accessToken");
     router.push("/");
-    setTimeout(() => {
-      window.location.reload();
-    }, 1000);
   };
 
-  useEffect(() => {
-    if (isUserAuthenticated() === true) {
-      setIsLoggedIn(true);
-    } else {
-      setIsLoggedIn(false);
-    }
-  }, [isUserAuthenticated]);
-
   return (
-    <div className="sticky top-0 bg-[#F7F5FD] flex justify-between items-center z-10 md:p-[10px] sm:p-[1px] h-[5rem] ">
-      <span
-        className="flex md:text-[18px] sm:text-[14px] font-bold cursor-pointer"
-        onClick={() => router.push("/")}
-      >
+    <div className="sticky top-0 bg-[#F7F5FD] flex justify-between items-center z-10  sm:p-[1px] h-auto ">
+      <span className="flex md:text-[18px] sm:text-[14px] font-bold cursor-pointer">
         CLIENT PORTAL
       </span>
-      <div className="flex gap-4 items-center">
-        <div className="border h-[3rem] rounded-md bg-white w-[20rem] flex items-center justify-center flex-col">
+      <div className="flex gap-4 items-center m-3">
+        <div className="border h-auto p-2 rounded-md bg-white w-auto flex items-center justify-center flex-col">
           <div className="flex items-center gap-2">
             <div className="p-2 border rounded-[50%] text-bold bg-slate-900 text-white items-center flex justify-center">
               NM
             </div>
-            <div>
-              <p className="font-bold text-[0.8rem] ">{user.entityName}</p>
+            <div className="flex gap-2 items-center">
+              <div>
+                <p className="font-bold text-[0.8rem] ">{user.entityName}</p>
+                <p className="font-bold text-[0.8rem] ">
+                  Category [{user.entityCodeName}]
+                </p>
+                <p className="font-bold text-[0.8rem] ">
+                  Code [{user.entityCode}]
+                </p>
+              </div>
+
+              <Menu>
+                <MenuButton
+                  as={Button}
+                  rightIcon={<IoChevronDownCircleOutline />}
+                ></MenuButton>
+                <MenuList>
+                  <div className="p-2">
+                    <p className="font-bold text-[0.8rem] ">
+                      {" "}
+                      Email accounts [{user.entityEmail}]
+                    </p>
+                    <p className="font-bold text-[0.8rem] ">
+                      Phone No [{user.entityPhone ? user.entityPhone : "N/A"}]
+                    </p>
+                    <p className="font-bold text-[0.8rem] ">
+                      Type [{user.entityType}]
+                    </p>
+                  </div>
+                </MenuList>
+              </Menu>
             </div>
           </div>
         </div>
         <CustomButton
           name="Logout"
-          className="h-[2rem] w-[5rem] bg-[#cb7529] rounded-md text-white"
+          onClick={handleLogout}
+          className="h-[2rem] w-[5rem]  bg-[#cb7529] rounded-md text-white"
         />
       </div>
     </div>
