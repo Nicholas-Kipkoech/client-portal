ef6db85a425d7d5e2c7e29821e26d441372db515
Nicholas Kipkoech <nickey968@gmail.com>
2024-06-28 15:06:59 +0300

    ....

--- app/context/claims/claims-context.tsx
+++ app/context/claims/claims-context.tsx
@@ -1,52 +1,52 @@
-"use client";
-import { getClaims } from "@/app/services/apiServices";
-import { ThemeContext } from "@emotion/react";
-import { jwtDecode } from "jwt-decode";
-import React, { createContext, useEffect, useState } from "react";
+'use client'
+import { getClaims } from '@/app/services/apiServices'
+import { ThemeContext } from '@emotion/react'
+import { jwtDecode } from 'jwt-decode'
+import React, { createContext, useEffect, useState } from 'react'
 
-const ClaimsContext = createContext({});
+const ClaimsContext = createContext({})
 
 export const ClaimsContextProvider = ({
   children,
 }: {
-  children: React.ReactNode;
+  children: React.ReactNode
 }) => {
-  const [claims, setClaims] = useState([]);
-  const [loadingClaims, setLoadingClaims] = useState(false);
-  const [user, setUser] = useState<any>({});
+  const [claims, setClaims] = useState([])
+  const [loadingClaims, setLoadingClaims] = useState(false)
+  const [user, setUser] = useState<any>({})
 
   useEffect(() => {
-    if (typeof window !== "undefined") {
-      const accessTokenJson: any = localStorage.getItem("accessToken");
+    if (typeof window !== 'undefined') {
+      const accessTokenJson: any = localStorage.getItem('accessToken')
       if (accessTokenJson) {
-        const decodedToken: any = jwtDecode(accessTokenJson);
-        setUser(decodedToken.payload);
+        const decodedToken: any = jwtDecode(accessTokenJson)
+        setUser(decodedToken.payload)
       }
     }
-  }, []);
+  }, [])
 
   useEffect(() => {
     async function fetchClaims() {
-      setLoadingClaims(true);
+      setLoadingClaims(true)
       if (Object.keys(user).length > 0) {
         const response = await getClaims({
           intermediaryCode: user?.intermediaryCode,
           clientCode: user?.entityCode,
-        });
-        setLoadingClaims(false);
-        setClaims(response.results);
+        })
+        setLoadingClaims(false)
+        setClaims(response.results)
       }
     }
-    fetchClaims();
-  }, [user]);
+    fetchClaims()
+  }, [user])
   const openClaims = claims.filter((claim: any) => {
-    return claim.status === "Open";
-  });
+    return claim.status === 'Open'
+  })
   return (
     <ClaimsContext.Provider value={{ claims, loadingClaims, openClaims }}>
       {children}
     </ClaimsContext.Provider>
-  );
-};
+  )
+}
 
-export default ClaimsContext;
+export default ClaimsContext
