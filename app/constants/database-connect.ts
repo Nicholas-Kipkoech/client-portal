988875bcee3f340452b0011310459f16e524bd9f
Nicholas Kipkoech <nickey968@gmail.com>
2024-07-01 15:12:35 +0300

    ...

--- app/constants/database-connect.ts
+++ app/constants/database-connect.ts
@@ -1,4 +1,4 @@
 // export const API_URL = "https://client-portal-backend-kqqf.onrender.com";
-export const API_URL = "http://localhost:5000";
+export const API_URL = "http://192.168.0.210:6000";
 
 export const _API_URL = "http://192.168.0.210:8101/icon/bima";
